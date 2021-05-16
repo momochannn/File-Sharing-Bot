b9257427ca5d8b772fec2b041bc794271a5bf4a9
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-05-16 19:40:07 +0700

    Update pyrogram.py

--- pyrogram.py
+++ pyrogram.py
@@ -1,3 +1,10 @@
+from pyrogram import Client
+
+api_id = 12345
+api_hash = "0123456789abcdef0123456789abcdef"
+
+with Client("my_account", api_id, api_hash) as app:
+    app.send_message("me", "Greetings from **Pyrogram**!")
 from pyrogram import Client, filters
 
 app = Client("my_account")
