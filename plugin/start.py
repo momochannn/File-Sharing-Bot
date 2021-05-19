8ffef392b93066bcc7b2978a64a907de05409aa8
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-05-19 09:11:16 +0700

    Update and rename plugins/start.py to plugin/start.py

--- plugin/start.py
+++ plugin/start.py
@@ -39,11 +39,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Harap tunggu ...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("Ada yang tidak beres ..!")
             return
         await temp_msg.delete()
         for msg in messages:
