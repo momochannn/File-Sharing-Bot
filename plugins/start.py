86e7c9b6e4757e87119dc00946964b8be214bc98
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-05-19 10:50:45 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -78,10 +78,10 @@ async def not_joined(client: Client, message: Message):
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>COBA KLIK LINK LAGI</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("CHANNEL 💌", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
