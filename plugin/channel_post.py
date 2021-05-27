e3b598577eb9ccb149da87cd34ff1a4d400901a9
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-28 05:22:16 +0700

    Update and rename plugins/channel_post.py to plugin/channel_post.py

--- plugin/channel_post.py
+++ plugin/channel_post.py
@@ -35,6 +35,7 @@ async def new_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    text = f'https://telegram.me/url?url={link}'
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
