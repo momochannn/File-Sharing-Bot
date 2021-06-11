67caf4d55ac1e94efcc00bda2cc11ff6cb296564
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-12 03:37:34 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -59,10 +59,8 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-               [
-               InlineKeyboardButton("⛔ TUTUP ⛔", callback_data = "close"),
-               InlineKeyboardButton("💌 JOIN CHANNEL 💌", url = client.invitelink)
-               ]
+               [InlineKeyboardButton("⛔ TUTUP ⛔", callback_data = "close")],
+               [InlineKeyboardButton("💌 JOIN CHANNEL 💌", url = client.invitelink)]
             ]
         )
         await message.reply_text(
