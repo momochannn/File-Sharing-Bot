be769736aca532e7380e27331ec88ac4f9f64739
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-11 21:15:10 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -73,7 +73,6 @@ async def start_command(client: Client, message: Message):
         )
         await message.reply_text(
             text = IKLAN
-            disable_web_page_preview = False,
         )
         return
 
