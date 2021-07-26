d841070c7e2021cf9acc7d1c619b7c6a64ebcd27
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-26 10:13:39 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,9 +84,6 @@ async def start_command(client: Client, message: Message):
             disable_web_page_preview = True,
             quote = True
         )
-        await message.reply(
-            text = IKLAN
-       )
         return
 
 @Bot.on_message(filters.command('start') & filters.private)
