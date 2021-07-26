517cb2f5ecd1e4103a00ef7d72f4e85b2eb0a6d3
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-26 08:42:01 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,7 +83,7 @@ async def not_joined(client: Client, message: Message):
         text = text + f" <b>Kalau belum join gak bisa buka file nya kalau sudah join silahkan klik refresh</b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("💌 JOIN CHANNEL 💌", url = client.invitelink), InlineKeyboardButton("🔄 REFRESH", url = f"https://t.me/{client.username}?start={argument}")]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("💌 JOIN CHANNEL 💌", url = client.invitelink)],[InlineKeyboardButton("🔄 REFRESH", url = f"https://t.me/{client.username}?start={argument}")]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
