106b43aa3293cca04a05a2c1cf14de759618ec1b
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-26 10:41:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+#username channel
+CHANNEL = os.environ.get("CHANNEL", "")
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
