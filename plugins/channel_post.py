07f5a3094bd391c8a13a1ace0b11a317beff3dd4
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-26 08:49:00 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -24,8 +24,8 @@ async def channel_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await reply_text.edit(f"<b>INI LINK ANDA</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇 SHARE", url=f'https://telegram.me/share/url?url={link}'), InlineKeyboardButton("📥 DOWNLOAD", url = f"{link}")]])
+    await reply_text.edit("SILAHKAN CEK TOMBOL DI BAWAH", reply_markup=reply_markup, disable_web_page_preview = True)
     await post_message.edit_reply_markup(reply_markup)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited)
@@ -34,7 +34,7 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇 SHARE", url=f'https://telegram.me/share/url?url={link}'), InlineKeyboardButton("📥 DOWNLOAD", url = f"{link}")]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
